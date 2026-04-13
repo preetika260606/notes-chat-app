bcea466c6cbc0940ca52183dc5288b04e3dbce45
preetikagit <preetikagupta329@gmail.com>
2026-04-13 22:20:42 +0530

    Updated navigation and documents logic, added item component

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,15 +1,22 @@
 "use client";
 
-import { ChevronLeft, MenuIcon } from "lucide-react";
+import { ChevronLeft, MenuIcon ,PlusCircle} from "lucide-react";
 import { usePathname } from "next/navigation";
 import { ElementRef,useEffect,useRef,useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
 import { cn } from "@/lib/utils";
 import { UserItem } from "./user-items";
+import { useQuery , useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { Item } from "./item";
+import { toast } from "sonner";
+
 
 export const Navigation =()=>{
     const pathname=usePathname();
     const isMobile=useMediaQuery("(max-width: 768px)");
+    const documents=useQuery(api.documents.get);
+    const create=useMutation(api.documents.create);
 
     const isResizingRef=useRef(false);
     const sidebarRef=useRef<ElementRef<"aside">>(null);
@@ -86,6 +93,16 @@ export const Navigation =()=>{
         }
     }
 
+    const handleCreate = () =>{
+        const promise = create({title:"Untitled"});
+
+        toast.promise(promise,{
+            loading:"Creating a new note",
+            success:"New note created!",
+            error:"Failed to create a new note."
+        });
+    }
+
     return (
         <>
         <aside 
@@ -108,9 +125,14 @@ export const Navigation =()=>{
             </div>
             <div>
                 <UserItem/>
+                <Item onClick={handleCreate} label="New Page" icon={PlusCircle}/>
             </div>
             <div className="mt-4">
-                <p>Documents</p>
+                {documents?.map((document)=>(
+                    <p key={document._id}>
+                        {document.title}
+                    </p>
+                ))}
             </div>
             <div 
             onMouseDown ={handleMouseDown}
