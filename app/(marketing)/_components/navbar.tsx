9f78373f42a738de8fedbdbcf31d40367e60d276
Preetika Gupta <2k23.csai2313671@gmail.com>
2026-08-31 22:04:14 +0530

    Fix Clerk UserButton compatibility

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -1,14 +1,22 @@
 "use client";
-import { useConvexAuth } from "convex/react";
-import { SignInButton,UserButton } from "@clerk/clerk-react";
-import { Link } from "lucide-react";
-import { useScrollTop } from "@/hooks/use-scroll-top";
-import { ModeToggle } from "@/components/mode-toggle";
-import { Button } from "@/components/ui/button";
-import { Spinner } from "@/components/spinner"
-import { cn } from "@/lib/utils";
-import { Logo } from "./logo";
 
+import { useConvexAuth } from "convex/react";
+
+import { SignInButton, UserButton } from "@clerk/nextjs";
+
+import Link from "next/link";
+
+import { useScrollTop } from "@/hooks/use-scroll-top";
+
+import { ModeToggle } from "@/components/mode-toggle";
+
+import { Button } from "@/components/ui/button";
+
+import { Spinner } from "@/components/spinner";
+
+import { cn } from "@/lib/utils";
+
+import { Logo } from "./logo";
 
 export const Navbar=()=>{
     const { isAuthenticated,isLoading} = useConvexAuth(); 
@@ -41,9 +49,7 @@ export const Navbar=()=>{
                         Enter Jotion
                         </Link>
                     </Button>
-                    <UserButton 
-                    afterSignOutUrl="/"
-                    />
+                    <UserButton />
                     </>
                 )}
                 <ModeToggle/>
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import { Toaster } from "sonner";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { ModalProvider } from "@/components/providers/modal-provider";
 import { ConvexClientProvider } from "@/components/providers/convex-provider";
